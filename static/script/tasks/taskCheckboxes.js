2671821812e7623368773a081ca28044ab835879
AndreSmalheer <driekie248@gmail.com>
2025-10-28 12:48:25 +0100

    failed task dont update xp or coins and now cheked mark apears

--- static/script/tasks/taskCheckboxes.js
+++ static/script/tasks/taskCheckboxes.js
@@ -4,7 +4,13 @@ export function initCheckboxes() {
   document.querySelectorAll(".task_checkbox").forEach((checkbox) => {
     checkbox.addEventListener("change", (event) => {
       const taskLabel = event.target.closest(".task_label");
+      const taskContainer = event.target.closest(".task");
       if (!taskLabel) return;
+
+      if (taskContainer.classList.contains("failed")) {
+        console.log("Task failed");
+        return;
+      }
 
       const task = taskLabel.closest(".task");
       if (!task.classList.contains("failed")) {
@@ -14,7 +20,6 @@ export function initCheckboxes() {
           task.classList.remove("completed");
         }
       }
-
       updateRewards(taskLabel, checkbox.checked);
     });
   });
