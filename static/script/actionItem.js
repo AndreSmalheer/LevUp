9ec1ddeace9daa15c6053be26d2b1efe5855ca8a
102464-AS <102464@glr.nl>
2025-11-04 09:32:53 +0100

    toggles class when clicking item

--- static/script/actionItem.js
+++ static/script/actionItem.js
@@ -1,15 +1,20 @@
 class Action {
-  constructor(label, checkbox = false, clickfunc = false, popup = false) {
+  constructor(
+    label,
+    checkbox = false,
+    clickfunc = false,
+    popup = false,
+    popupData = ""
+  ) {
     this.label = label;
     this.checkbox = checkbox;
     this.clickfunc = clickfunc;
     this.popup = popup;
+    this.popupData = popupData;
   }
 
   create() {
     const container = document.getElementById("tasks_container");
-
-    // Create main wrapper
     const actionDiv = document.createElement("div");
     actionDiv.classList.add("task");
 
@@ -17,40 +22,48 @@ class Action {
     actionLabel.classList.add("task_label");
     actionDiv.appendChild(actionLabel);
 
-    // Add checkbox (if enabled)
     if (this.checkbox) {
       const input = document.createElement("input");
       input.type = "checkbox";
       input.classList.add("task_checkbox");
+      input.addEventListener("click", (event) => {
+        event.stopPropagation();
+        const taskContainer = event.target.closest(".task");
+        if (event.target.checked) {
+          taskContainer.classList.add("completed");
+        } else {
+          taskContainer.classList.remove("completed");
+        }
+      });
       actionLabel.appendChild(input);
     }
 
-    // Add label text
     const labelSpan = document.createElement("span");
     labelSpan.classList.add("task_text");
     labelSpan.textContent = this.label;
     actionLabel.appendChild(labelSpan);
 
-    // Add click function (if provided)
     if (this.clickfunc) {
       actionDiv.addEventListener("click", this.clickfunc);
     }
 
-    // Add popup (if true)
     if (this.popup) {
-      actionDiv.addEventListener("click", () => {
-        alert(`Popup for: ${this.label}`);
-      });
+      pass;
     }
 
-    // Add to container
     container.appendChild(actionDiv);
   }
 }
 
 function hello() {
-  console.log("hey");
+  console.log("Task clicked!");
 }
 
-const action1 = new Action("Test Action", true, hello);
-action1.create();
+const action = new Action(
+  "Do the dishes",
+  false,
+  false,
+  true,
+  "<ul><li>Test</li></ul>"
+);
+action.create();
