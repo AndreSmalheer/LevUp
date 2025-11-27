b9d09f3eaf150fb02b5426c88f893528abc24135
andre <driekie248@gmail.com>
2025-11-27 11:05:39 +0100

    cleanded up python files

---
+++ models/task.py
@@ -0,0 +1,24 @@
+from models.db import get_connection
+
+def get_tasks():
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM tasks")
+    rows = cursor.fetchall()
+    tasks = [{
+        "task_id": row["id"],
+        "task_name": row["task_name"],
+        "coin_reward": row["coin_reward"],
+        "xp_reward": row["xp_reward"],
+        "start_time": row["start_time"],
+        "end_time": row["end_time"],
+        "completed": bool(row["completed"]),
+        "failed": bool(row['failed']),
+        "penelty_id": (row['penelty_id']),
+        "repeat_days": row["repeat_days"]
+    } for row in rows]
+    cursor.close()
+    conn.close()
+    return tasks
+
+
